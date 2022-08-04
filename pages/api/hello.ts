d5adfed1d95f2b2ca9b4e77e55a847909a316873
Михаил Пугачев <xd383727@gmail.com>
2022-08-05 00:33:22 +0300

    test interface

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,16 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-type Data = {
-  name: string
+export interface toDo{
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+function getNew(): Promise<toDo[]>{
+  return fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(res => res.json())
+  .then(res=>{
+    return res as toDo[]
+  })
 }
+
+export default getNew;
